0872e5422c6399b2bf1b3cd93edb3c9247ba06eb
jspenc72 <jspenc72@gmail.com>
2014-02-05 23:18:41 -0700

    Added network status detection on the login screen.
    Added network status detection on the login screen.

--- ubmchangerequest/platforms/ios/.staging/www/cordova_plugins.js
+++ ubmchangerequest/platforms/ios/.staging/www/cordova_plugins.js
@@ -170,6 +170,21 @@ module.exports = [
         "clobbers": [
             "CameraPopoverHandle"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.network-information/www/network.js",
+        "id": "org.apache.cordova.network-information.network",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.network-information/www/Connection.js",
+        "id": "org.apache.cordova.network-information.Connection",
+        "clobbers": [
+            "Connection"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -177,7 +192,8 @@ module.exports.metadata =
 {
     "org.apache.cordova.file": "0.2.5",
     "org.apache.cordova.file-transfer": "0.4.0",
-    "org.apache.cordova.camera": "0.2.6"
+    "org.apache.cordova.camera": "0.2.6",
+    "org.apache.cordova.network-information": "0.2.6"
 }
 // BOTTOM OF METADATA
 });
